6f437bfe8cff20c80b7be5cd3f0da457f6d980b3
tmaheshbabu126 <tmaheshbabu126@gmail.com>
2022-02-03 15:05:49 +0530

    created and adjusted all the UI and made everything functional added upload export generate functionality

--- SprintTask5/src/main/java/com/Controller/FileController.java
+++ SprintTask5/src/main/java/com/Controller/FileController.java
@@ -1,11 +1,21 @@
 package com.Controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.springframework.http.HttpHeaders;
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -45,4 +55,55 @@ public class FileController {
 
         return "redirect:/admin";
     }
+    
+    @RequestMapping(value = "/download", method = RequestMethod.GET) 
+    public ResponseEntity<Object> downloadFile() throws IOException  {
+       String filename = "C:\\Users\\mahes\\Desktop\\Task-CG-Asha\\Files\\Input File\\baby.jfif";
+       File file = new File(filename);
+       InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+       HttpHeaders headers = new HttpHeaders();
+       
+       headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+       headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+       headers.add("Pragma", "no-cache");
+       headers.add("Expires", "0");
+       
+       ResponseEntity<Object> 
+       responseEntity = ResponseEntity.ok().headers(headers).contentLength(
+          file.length()).contentType(MediaType.parseMediaType("application/txt")).body(resource);
+       
+      
+       
+       return responseEntity;
+    }
+    
+    @RequestMapping(value = "/generate", method = RequestMethod.GET)
+    public String create_file(RedirectAttributes redirectAttributes) throws IOException  {
+    	final String FILE_NAME = "C://Users/mahes/Desktop/Task-CG-Asha/Files/Output File/CreatedFile.csv";
+        File newFile = new File(FILE_NAME);
+        boolean success = newFile.createNewFile();
+        //InputStreamResource resource = new InputStreamResource(new FileInputStream(newFile));
+        if(success) System.out.println("File created");
+        else System.out.println("already file exist please remove it if want to create again");
+        
+//        HttpHeaders headers = new HttpHeaders();
+//        
+//        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", newFile.getName()));
+//        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+//        headers.add("Pragma", "no-cache");
+//        headers.add("Expires", "0");
+//        
+//        ResponseEntity<Object> 
+//        responseEntity = ResponseEntity.ok().headers(headers).contentLength(
+//           newFile.length()).contentType(MediaType.parseMediaType("application/txt")).body(resource);
+//        
+//        return responseEntity;
+        
+        redirectAttributes.addFlashAttribute("message",
+                "You have successfully Exported the file!");
+        return "redirect:/admin";
+    }
+    
+    
+    
 }
